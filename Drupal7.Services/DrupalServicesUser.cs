e984479ad909e49aa187fb0cb5a013709061b6ba
Tito Brasolin <tito.brasolin@gmail.com>
2014-12-12 15:49:39 +0100

    Merge struct DrupalUser into DrupalServicesUser.cs

--- Drupal7.Services/DrupalServicesUser.cs
+++ Drupal7.Services/DrupalServicesUser.cs
@@ -7,6 +7,28 @@ namespace Drupal7.Services
 		public string token;
 	}
 
+	[XmlRpcMissingMapping(MappingAction.Ignore)]
+	public struct DrupalUser
+	{
+		public object uid;
+		public object login;
+		public string access;
+		public string init;
+		public string signature;
+		public string name;
+		public string signature_format;
+		public string theme;
+		public string timezone;
+		public object data;
+		public string created;
+		public object picture;
+		public string status;
+		public XmlRpcStruct rdf_mapping;
+		public XmlRpcStruct roles;
+		public string language;
+		public string mail;
+	}
+
 	/// <summary>
 	/// https://www.drupal.org/project/services
 	/// </summary>
