8047e7688c135a6da497d57312bb2f7b0f48e895
Tito Brasolin <tito.brasolin@gmail.com>
2014-12-12 15:40:32 +0100

    Merging struct DrupalFile into DrupalServicesFile.cs

--- Drupal7.Services/DrupalServicesFile.cs
+++ Drupal7.Services/DrupalServicesFile.cs
@@ -2,6 +2,26 @@
 using CookComputing.XmlRpc;
 namespace Drupal7.Services
 {
+	[XmlRpcMissingMapping(MappingAction.Ignore)]
+	public struct DrupalFile
+	{
+		public string type;
+		public string target_uri;
+		public string uri;
+		public string file;
+		public string origname;
+		public string filesize;
+		public string filemime;
+		public string uid;
+		public string status;
+		public object[] rdf_mapping;
+		public string timestamp;
+		public string uri_full;
+		public string filename;
+		public string fid;
+		public object image_styles;
+	}
+
 	/// <summary>
 	/// https://www.drupal.org/project/services
 	/// </summary>
