ec6297fdee7e10eba8011ce7376b9368993f7b5a
Tito Brasolin <tito.brasolin@gmail.com>
2014-12-14 15:37:36 +0100

    Refactoring.

--- Drupal7.Services/DrupalServices.cs
+++ Drupal7.Services/DrupalServices.cs
@@ -8,6 +8,15 @@ using CookComputing.XmlRpc;
 
 namespace Drupal7.Services
 {
+	[XmlRpcMissingMapping(MappingAction.Ignore)]
+	public struct DrupalSessionObject
+	{
+		public string sessid;
+		public string session_name;
+		public string token;
+		public DrupalUser user;
+	}
+
 	public sealed partial class DrupalServices
 	{
 		public delegate void HandledExceptionDelegate(Exception ex, string functionName);
