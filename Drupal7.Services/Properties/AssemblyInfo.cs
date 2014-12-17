293db416b1b17e572beb0e2c3283f6199207dcbf
Tito Brasolin <tito.brasolin@gmail.com>
2014-12-17 22:32:50 +0100

    Removing unneeded AssemblyInformationalVersion.

--- Drupal7.Services/Properties/AssemblyInfo.cs
+++ Drupal7.Services/Properties/AssemblyInfo.cs
@@ -18,7 +18,6 @@ using System.Runtime.CompilerServices;
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
 [assembly: AssemblyVersion("0.0.6")]
-[assembly: AssemblyInformationalVersion("0.0.6-alpha")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
