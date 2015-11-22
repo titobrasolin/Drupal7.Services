4c2f0dfd1d52ba47594673407f9a209b7415aea7
Tito Brasolin <tito.brasolin@gmail.com>
2015-11-22 11:52:42 +0100

    New DrupalServicesAddressField.cs file.

---
+++ Drupal7.Services/DrupalServicesAddressField.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using CookComputing.XmlRpc;
+namespace Drupal7.Services
+{
+	/// <summary>
+	/// https://www.drupal.org/project/services_addressfield
+	/// </summary>
+	public sealed partial class DrupalServices
+	{
+		/// <summary>
+		/// Get list of all predefined and custom countries.
+		/// </summary>
+		/// <returns>List of all predefined and custom countries.</returns>
+		public XmlRpcStruct AddressFieldCountryGetList()
+		{
+			this.InitRequest();
+			XmlRpcStruct res = null;
+			try {
+				res = drupalServiceSystem.AddressFieldCountryGetList();
+			} catch (Exception ex) {
+				this.HandleException(ex, "AddressFieldCountryGetList");
+			}
+			return res;
+		}
+
+		/// <summary>
+		/// Returns the predefined administrative areas for the given country code.
+		/// </summary>
+		/// <param name="country_code">The country code.</param>
+		/// <returns>The predefined administrative areas for the given country code.</returns>
+		public XmlRpcStruct AddressFieldGetAddressFormatAndAdministrativeAreas(string country_code)
+		{
+			this.InitRequest();
+			XmlRpcStruct res = null;
+			try {
+				res = drupalServiceSystem.AddressFieldGetAddressFormatAndAdministrativeAreas(country_code);
+			} catch (Exception ex) {
+				this.HandleException(ex, "AddressFieldGetAddressFormatAndAdministrativeAreas");
+			}
+			return res;
+		}
+	}
+}
