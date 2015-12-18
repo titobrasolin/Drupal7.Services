af14cdb1f967fe76e5a47fb163f68611272636e2
Tito Brasolin <tito.brasolin@gmail.com>
2015-12-18 10:42:09 +0100

    Adding BaseNode class.

---
+++ Drupal7.Services/BaseClasses/BaseNode.cs
@@ -0,0 +1,202 @@
+﻿namespace Drupal7.Services.BaseClasses
+{
+    class BaseNode
+    {
+        private int _nid;
+        private int _promote;
+        private int _changed;
+        private string _type;
+        private int _created;
+        private int _comment;
+        private string _title;
+        private int _sticky;
+        private int _tnid;
+        private int _uid;
+        private int _vid;
+        private int _status;
+        private int _translate;
+        private string _language;
+
+        public int nid
+        {
+            get
+            {
+                return _nid;
+            }
+
+            set
+            {
+                _nid = value;
+            }
+        }
+
+        public int promote
+        {
+            get
+            {
+                return _promote;
+            }
+
+            set
+            {
+                _promote = value;
+            }
+        }
+
+        public int changed
+        {
+            get
+            {
+                return _changed;
+            }
+
+            set
+            {
+                _changed = value;
+            }
+        }
+
+        public string type
+        {
+            get
+            {
+                return _type;
+            }
+
+            set
+            {
+                _type = value;
+            }
+        }
+
+        public int created
+        {
+            get
+            {
+                return _created;
+            }
+
+            set
+            {
+                _created = value;
+            }
+        }
+
+        public int comment
+        {
+            get
+            {
+                return _comment;
+            }
+
+            set
+            {
+                _comment = value;
+            }
+        }
+
+        public string title
+        {
+            get
+            {
+                return _title;
+            }
+
+            set
+            {
+                _title = value;
+            }
+        }
+
+        public int sticky
+        {
+            get
+            {
+                return _sticky;
+            }
+
+            set
+            {
+                _sticky = value;
+            }
+        }
+
+        public int tnid
+        {
+            get
+            {
+                return _tnid;
+            }
+
+            set
+            {
+                _tnid = value;
+            }
+        }
+
+        public int uid
+        {
+            get
+            {
+                return _uid;
+            }
+
+            set
+            {
+                _uid = value;
+            }
+        }
+
+        public int vid
+        {
+            get
+            {
+                return _vid;
+            }
+
+            set
+            {
+                _vid = value;
+            }
+        }
+
+        public int status
+        {
+            get
+            {
+                return _status;
+            }
+
+            set
+            {
+                _status = value;
+            }
+        }
+
+        public int translate
+        {
+            get
+            {
+                return _translate;
+            }
+
+            set
+            {
+                _translate = value;
+            }
+        }
+
+        public string language
+        {
+            get
+            {
+                return _language;
+            }
+
+            set
+            {
+                _language = value;
+            }
+        }
+    }
+}
