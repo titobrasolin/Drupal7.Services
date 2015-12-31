e9aab231d7079c5c168f8f8323cde550895b95cc
Tito Brasolin <tito.brasolin@gmail.com>
2015-12-31 17:56:39 +0100

    Added a couple of classes to model common field types.

---
+++ Drupal7.Services/FieldTypes/Text.cs
@@ -0,0 +1,9 @@
+﻿namespace Drupal7.Services.FieldTypes
+{
+    class Text
+    {
+        public string value { get; set; }
+        public string format { get; set; }
+        public string safe_value { get; set; }
+    }
+}
---
+++ Drupal7.Services/FieldTypes/TextWithSummary.cs
@@ -0,0 +1,8 @@
+﻿namespace Drupal7.Services.FieldTypes
+{
+    class TextWithSummary : Text
+    {
+        public string summary { get; set; }
+        public string safe_summary { get; set; }
+    }
+}
