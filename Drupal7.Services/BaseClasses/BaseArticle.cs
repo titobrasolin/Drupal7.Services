752c16156aa32153d1df2f245e43616b2318f819
Tito Brasolin <tito.brasolin@gmail.com>
2016-01-03 14:43:02 +0100

    New BaseArticle class (draft).

---
+++ Drupal7.Services/BaseClasses/BaseArticle.cs
@@ -0,0 +1,24 @@
+﻿using Drupal7.Services.FieldTypes;
+
+namespace Drupal7.Services.BaseClasses
+{
+    public class BaseArticle : BaseNode
+    {
+        public object rdf_mapping { get; set; }
+        public string data { get; set; }
+        public int picture { get; set; }
+        public int last_comment_timestamp { get; set; }
+        // public ... field_tags { get; set; }
+        public string last_comment_name { get; set; }
+        // public ... log { get; set; }
+        public int cid { get; set; }
+        public int revision_uid { get; set; }
+        public int comment_count { get; set; }
+        public string path { get; set; }
+        public ImageField field_image { get; set; }
+        public TextWithSummaryField body { get; set; }
+        public int revision_timestamp { get; set; }
+        public int last_comment_uid { get; set; }
+        public string name { get; set; }
+    }
+}
