faa61ed84552e53b157ae1632843522c2bee3906
Tito Brasolin <tito.brasolin@gmail.com>
2016-01-03 14:02:58 +0100

    New class for image field type.

---
+++ Drupal7.Services/FieldTypes/Image.cs
@@ -0,0 +1,19 @@
+﻿namespace Drupal7.Services.FieldTypes
+{
+    class Image
+    {
+        public string filename { get; set; }
+        public string alt { get; set; }
+        public string filemime { get; set; }
+        public int fid { get; set; }
+        public int filesize { get; set; }
+        public object[] rdf_mapping { get; set; }
+        public int height { get; set; }
+        public string title { get; set; }
+        public string uri { get; set; }
+        public int timestamp { get; set; }
+        public int uid { get; set; }
+        public int status { get; set; }
+        public int width { get; set; }
+    }
+}
