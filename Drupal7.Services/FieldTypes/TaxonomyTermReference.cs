95031c2e1042b7b74a73652c064c81b4b0cae290
Tito Brasolin <tito.brasolin@gmail.com>
2016-01-03 16:16:42 +0100

    Draft code for taxonomy term reference field.

---
+++ Drupal7.Services/FieldTypes/TaxonomyTermReference.cs
@@ -0,0 +1,11 @@
+﻿namespace Drupal7.Services.FieldTypes
+{
+    public class TaxonomyTid
+    {
+        public int tid { get; set; }
+    }
+    public class TaxonomyTermReferenceField
+    {
+        public TaxonomyTid[] und { get; set; }
+    }
+}
