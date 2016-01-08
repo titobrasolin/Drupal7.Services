8b1d45a397077e0ca2069f47a3f38a3420678af8
Tito Brasolin <tito.brasolin@gmail.com>
2016-01-08 23:27:37 +0100

    Modeling date field with end date and repetition. Some fixes in BaseUser.

--- Drupal7.Services/BaseClasses/BaseUser.cs
+++ Drupal7.Services/BaseClasses/BaseUser.cs
@@ -1,18 +1,17 @@
-﻿using System;
-using System.Web.Script.Serialization;
-namespace Drupal7.Services
+﻿namespace Drupal7.Services
 {
     public class BaseUser
     {
         public string signature { get; set; }
-        public string data { get; set; }
+        public object data { get; set; }
+        public object roles { get; set; }
         public int access { get; set; }
         public string init { get; set; }
         public string pass { get; set; }
         public int created { get; set; }
         public string mail { get; set; }
         public string signature_format { get; set; }
-        public int picture { get; set; }
+        public string picture { get; set; }
         public string language { get; set; }
         public int login { get; set; }
         public string timezone { get; set; }
@@ -20,5 +19,6 @@ namespace Drupal7.Services
         public string theme { get; set; }
         public int uid { get; set; }
         public string name { get; set; }
+        public string services_token { get; set; }
     }
 }
---
+++ Drupal7.Services/FieldTypes/DateTime.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Drupal7.Services
+{
+    public class DateTimeField
+    {
+        public IList<DateTimeFieldItem> und { get; set; }
+    }
+    public class DateTimeFieldItem
+    {
+        public string date_type { get; set; }
+        public string timezone { get; set; }
+        public string timezone_db { get; set; }
+        public string value { get; set; }
+        public string value2 { get; set; }
+        public string rrule { get; set; }
+    }
+}
