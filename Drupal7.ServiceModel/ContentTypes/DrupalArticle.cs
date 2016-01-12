b29a7ac02956f9819988c1b84299902b6f954e28
Tito Brasolin <tito.brasolin@gmail.com>
2016-01-12 20:24:12 +0100

    Moving and renaming model class for Drupal article

---
+++ Drupal7.ServiceModel/ContentTypes/DrupalArticle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Drupal7.ServiceModel.BaseClasses;
+using Drupal7.ServiceModel.FieldTypes;
+
+namespace Drupal7.ServiceModel.ContentTypes
+{
+    public class DrupalArticle : BaseNode
+    {
+        public Dictionary<string, List<ImageFieldItem>> field_image { get; set; }
+        public Dictionary<string, List<TaxonomyTermReferenceFieldItem>> field_tags { get; set; }
+        public Dictionary<string, List<TextFieldItem>> body { get; set; }
+    }
+}
+
