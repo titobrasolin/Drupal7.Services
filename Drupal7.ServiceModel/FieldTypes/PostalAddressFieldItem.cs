e3f4ce68c812bfae84de4b125050e53e29127d34
Tito Brasolin <tito.brasolin@gmail.com>
2016-01-13 20:25:25 +0100

    Adding PostalAddressFieldItem.

---
+++ Drupal7.ServiceModel/FieldTypes/PostalAddressFieldItem.cs
@@ -0,0 +1,20 @@
+﻿namespace Drupal7.ServiceModel.FieldTypes
+{
+    public class PostalAddressFieldItem
+    {
+        public string country { get; set; }
+        public string administrative_area { get; set; }
+        public string sub_administrative_area { get; set; }
+        public string locality { get; set; }
+        public string dependent_locality { get; set; }
+        public string postal_code { get; set; }
+        public string thoroughfare { get; set; }
+        public string premise { get; set; }
+        public string sub_premise { get; set; }
+        public string organisation_name { get; set; }
+        public string name_line { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public object data { get; set; }
+    }
+}
